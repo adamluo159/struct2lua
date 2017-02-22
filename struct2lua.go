08a8f08fab723876cae80a40680cba710cdcb779
adamluo159 <15966536@qq.com>
2017-02-22 22:47:05 +0800

    修改：      README.md 	新文件：    struct2lua.go

---
+++ struct2lua.go
@@ -0,0 +1,117 @@
+package struct2lua
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+type CWD struct {
+	A string
+	B int
+	C []int
+	D []string
+	E bool
+}
+
+type Log struct {
+	CW CWD
+	ID int
+	IP string
+}
+
+func spaceLayer(layer int) string {
+	var result string = ""
+	for i := 0; i < layer; i++ {
+		result += "\t"
+	}
+	return result
+}
+
+func toLuaObject(layer int, k *reflect.StructField, v reflect.Value) string {
+	var result string = ""
+
+	var kName string = spaceLayer(layer)
+	if k != nil {
+		kName = kName + k.Name + " = "
+	}
+
+	switch v.Kind() {
+	case reflect.Struct:
+		result += kName + "{"
+		prefix := "\n"
+		for i := 0; i < v.NumField(); i++ {
+			subK := k.Type.Field(i)
+			result += prefix + toLuaObject(layer+1, &subK, v.Field(i))
+			prefix = ",\n"
+		}
+		result += "\n" + spaceLayer(layer) + "}"
+
+	case reflect.Slice:
+		result += kName + "{"
+		prefix := "\n"
+
+		for i := 0; i < v.Len(); i++ {
+			result += prefix + toLuaObject(layer+1, nil, v.Index(i))
+			prefix = ",\n"
+		}
+		result += "\n" + spaceLayer(layer) + "}"
+
+	case reflect.String:
+		s := v.String()
+		if s == "" {
+			result = kName + "nil"
+		} else {
+			result = kName + "\"" + s + "\""
+		}
+
+	case reflect.Int:
+		vInt := (int)(v.Int())
+		str := strconv.Itoa(vInt)
+		result = kName + str
+
+	case reflect.Bool:
+		var b bool = v.Bool()
+		result = kName + strconv.FormatBool(b)
+
+	default:
+		result = "nil"
+	}
+
+	return result
+}
+
+func ToLuaConfig(obj interface{}) string {
+	k := reflect.TypeOf(obj)
+	v := reflect.ValueOf(obj)
+	if v.Kind() != reflect.Struct {
+		fmt.Println("this data is not struct!!")
+		return "false"
+	}
+
+	layer := 0
+	head := spaceLayer(layer) + k.Name() + "= {\n"
+
+	for i := 0; i < k.NumField(); i++ {
+		k1 := k.Field(i)
+		head += toLuaObject(layer+1, &k1, v.Field(i)) + ",\n"
+	}
+	head += spaceLayer(layer) + "}"
+	return head
+}
+
+//func main() {
+//	logsrv := Log{
+//		CW: CWD{
+//			A: "aaaaa",
+//			B: 123,
+//			C: []int{1, 2, 3},
+//			D: []string{"dfdfdfddf", "5", "6"},
+//			E: true,
+//		},
+//		ID: 1,
+//		IP: "192.168.1.1",
+//	}
+//	result := ToLuaConfig(logsrv)
+//	fmt.Println(result)
+//}
